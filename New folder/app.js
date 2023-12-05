442c8756d1f51eb266fcb0705969c2a07e92b6ee
Rehan Pasha <104275485+Rehanpasha732@users.noreply.github.com>
2023-12-05 10:54:39 -0800

    my-project

---
+++ New folder/app.js
@@ -0,0 +1,60 @@
+// var name;
+// name = 'rehan pasha'
+// console.log(name)
+
+// var array = ['jan','march','apr','may']
+
+//  array.splice(1,0,'feb')
+// console.log(array)
+
+// var array = ['jan', 'march', 'apr', 'may']
+// var newA = array.slice(0,4)
+// console.log(newA)
+
+// var array = ['jan', 'march', 'apr', 'may']
+
+// array.map((v,i)=>{
+// console.log(v,i)
+// })
+
+// var array = [1,4,5,7,4]
+// var newNum = array.includes(3)
+// console.log(newNum)
+
+// var array = [1, 4, 5, 7, 4]
+// var newNumSort = array.sort()
+// console.log(newNumSort)
+
+// setTimeout(()=>{
+// console.log('hello')
+// },1000)
+
+// setInterval(() => {
+//     console.log('hello this is interval')
+// }, 1000)
+
+// function name(params) {
+
+// }
+
+// function name(params) {
+
+// }
+
+
+var array = [{
+    name: 'rehan',
+    email: 'rehan1233'
+},
+{
+    name: 'noman',
+    email: 'noman@gmail.com'
+}
+
+]
+
+array.map((v, i) => {
+    console.log(v.name)
+    console.log(v.email)
+
+})
